e424635b6ac491d0e70ff2206c2a0087303e396e
vivilis85 <vivipaez381@gmail.com>
2022-11-11 17:05:05 -0500

    credito22

---
+++ app/src/main/java/com/example/credito/MenuActivity.java
@@ -0,0 +1,4 @@
+package com.example.credito;
+
+public class MenuActivity {
+}
--- app/src/main/java/com/example/credito/UsuarioActivity.java
+++ app/src/main/java/com/example/credito/UsuarioActivity.java
@@ -3,6 +3,7 @@ package com.example.credito;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.ContentValues;
+import android.content.Intent;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
@@ -20,13 +21,14 @@ public class UsuarioActivity extends AppCompatActivity {
     ClsOpenHelper admin=new ClsOpenHelper(this,"Banco.bd",null,1);
 
     String identificacion, nombre, profesion, empresa, salario, extras, gastos;
-    Long resp;
+    long resp;
+    byte sw;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_usuario);
-        //ocultar nombre app y asociar objetos
+        //ocultar barra de titulo y asociar objetos Java y Xml
         getSupportActionBar().hide();
         jetidentificacion=findViewById(R.id.etidentificacion);
         jetnombre=findViewById(R.id.etnombre);
@@ -36,7 +38,9 @@ public class UsuarioActivity extends AppCompatActivity {
         jetextras=findViewById(R.id.etextras);
         jetgastos=findViewById(R.id.etgastos);
         jcbactivo=findViewById(R.id.cbactivo);
+        sw=0;
     }
+
     public void Guardar(View view){
         identificacion=jetidentificacion.getText().toString();
         nombre=jetnombre.getText().toString();
@@ -45,11 +49,12 @@ public class UsuarioActivity extends AppCompatActivity {
         salario=jetsalario.getText().toString();
         extras=jetextras.getText().toString();
         gastos=jetgastos.getText().toString();
-        if (identificacion.isEmpty()|| nombre.isEmpty() || profesion.isEmpty() || empresa.isEmpty() || salario.isEmpty() || extras.isEmpty() || gastos.isEmpty()){
-            Toast.makeText(this, "Todos los datos son obligatorios", Toast.LENGTH_SHORT).show();
+        if (identificacion.isEmpty() || nombre.isEmpty() || profesion.isEmpty() ||
+                empresa.isEmpty() || salario.isEmpty() || extras.isEmpty() || gastos.isEmpty()){
+            Toast.makeText(this, "Todos los datos son requeridos", Toast.LENGTH_SHORT).show();
             jetidentificacion.requestFocus();
         }
-        else {
+        else{
             SQLiteDatabase fila=admin.getWritableDatabase();
             ContentValues registro=new ContentValues();
             registro.put("identificacion",identificacion);
@@ -59,56 +64,100 @@ public class UsuarioActivity extends AppCompatActivity {
             registro.put("salario",Integer.parseInt(salario));
             registro.put("ingreso_extra",Integer.parseInt(extras));
             registro.put("gastos",gastos);
-            resp=fila.insert("TblCliente",null,registro);
-            if (resp==0){
-                Toast.makeText(this, "Erro guardando registro", Toast.LENGTH_SHORT).show();
-            }else {
-                Toast.makeText(this, "Registro exitoso", Toast.LENGTH_SHORT).show();
+            if (sw == 0)
+                resp=fila.insert("TblCliente",null,registro);
+            else {
+                resp = fila.update("TblCliente", registro, "identificacion='" + identificacion + "'", null);
+                sw=0;
+            }
+            if (resp == 0){
+                Toast.makeText(this, "Error guardando registro", Toast.LENGTH_SHORT).show();
+            }
+            else{
+                Toast.makeText(this, "Registro guardado", Toast.LENGTH_SHORT).show();
                 Limpiar_campos();
             }
             fila.close();
         }
     }
 
-    public void Consultar(View view){
-        identificacion = jetidentificacion.getText().toString();
-        if(identificacion.isEmpty()){
-            Toast.makeText(this, "Identificacion es requerida por la consulta", Toast.LENGTH_SHORT).show();
+    public void Buscar(View view){
+        identificacion=jetidentificacion.getText().toString();
+        if (identificacion.isEmpty()){
+            Toast.makeText(this, "Identificacion es requerida para la consulta", Toast.LENGTH_SHORT).show();
             jetidentificacion.requestFocus();
-
         }
         else{
-            SQLiteDatabase fila = admin.getWritableDatabase();
-            Cursor dato = fila.rawQuery("select * from TblCliente where identificacion= '" + identificacion + "'",null);
-            if(dato.moveToNext()){
-                jetnombre.setText(dato.getString( 1));
-                jetprofesion.setText(dato.getString( 2));
-                jetempresa.setText(dato.getString( 3));
-                jetsalario.setText(dato.getString( 4));
-                jetextras.setText(dato.getString( 5));
-                jetgastos.setText(dato.getString( 6));
-
-                if(dato.getString(7).equals("si"))
+            SQLiteDatabase fila=admin.getReadableDatabase();
+            Cursor dato=fila.rawQuery("select * from TblCliente where identificacion='" + identificacion + "'",null);
+            if (dato.moveToNext()){
+                sw=1;
+                jetnombre.setText(dato.getString(1));
+                jetprofesion.setText(dato.getString(2));
+                jetempresa.setText(dato.getString(3));
+                jetsalario.setText(dato.getString(4));
+                jetextras.setText(dato.getString(5));
+                jetgastos.setText(dato.getString(6));
+                if (dato.getString(7).equals("si"))
                     jcbactivo.setChecked(true);
-
-                    else
-                        jcbactivo.setChecked(false);
-
-
-
-
-
+                else
+                    jcbactivo.setChecked(false);
+            }
+            else{
+                Toast.makeText(this, "Cliente no registrado", Toast.LENGTH_SHORT).show();
+                jetidentificacion.requestFocus();
             }
         }
     }
+
+    public void Anular(View view){
+        if (sw == 0){
+            Toast.makeText(this, "Debe primero consultar para anular", Toast.LENGTH_SHORT).show();
+            jetidentificacion.requestFocus();
+        }
+        else{
+            identificacion=jetidentificacion.getText().toString();
+            if (identificacion.isEmpty()){
+                Toast.makeText(this, "La identificacion es requerida", Toast.LENGTH_SHORT).show();
+                jetidentificacion.requestFocus();
+            }
+            else{
+                sw = 0;
+                SQLiteDatabase fila = admin.getWritableDatabase();
+                ContentValues registro = new ContentValues();
+                registro.put("activo", "no");
+                resp = fila.update("TblCliente", registro, "identificacion='" + identificacion + "'", null);
+                if (resp == 0) {
+                    Toast.makeText(this, "Error anulando registro", Toast.LENGTH_SHORT).show();
+                } else {
+                    Toast.makeText(this, "Registro anulado", Toast.LENGTH_SHORT).show();
+                    Limpiar_campos();
+                }
+                fila.close();
+            }
+        }
+    }
+
+    public void Regresar(View view){
+        Intent intmenu=new Intent(this,MenuActivity.class);
+        startActivity(intmenu);
+    }
+
+    public void Cancelar(View view){
+        Limpiar_campos();
+    }
+
     private void Limpiar_campos(){
         jetidentificacion.setText("");
-        jetnombre.setText("");
-        jetprofesion.setText("");
-        jetempresa.setText("");
-        jetsalario.setText("");
-        jetextras.setText("");
         jetgastos.setText("");
-        jcbactivo.setText("");
+        jetextras.setText("");
+        jetsalario.setText("");
+        jetempresa.setText("");
+        jetprofesion.setText("");
+        jetnombre.setText("");
+        jcbactivo.setChecked(false);
+        jetidentificacion.requestFocus();
+        sw=0;
     }
+
 }
